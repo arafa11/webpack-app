168a70583ba2879f7d3874e67d34aa4cfc60e6a7
Mahmoud Arafa <ma.arafa251@gmail.com>
2021-09-26 17:57:12 +0200

    cache busting, plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
   // devtool: "none", // remove eval from output
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js", // contentHash
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -20,4 +21,8 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html",
+  })],
 }
+
